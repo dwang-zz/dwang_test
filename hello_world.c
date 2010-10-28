5825d8d0ec0a39eee8cbe10027585ea5532d5931
Dengyi Wang <dengyi.wang@windriver.com>
2010-10-28 13:57:22 -0400

    add hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+ * 
+ * Author: Dengyi Wang
+ * Created Time: Thu 28 Oct 2010 01:54:50 PM EDT
+ * File Name: hello_world.c
+ * Description: 
+ * 
+ ************************************************************************/
+
+#include <stdio.h>
+
+#define VER_NUM 1
+
+int main()
+{
+    printf("Hello World: %d\n", VER_NUM);
+
+    return 0;
+}
